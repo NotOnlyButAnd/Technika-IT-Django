a4eeaff5ec3ece1815296940f8f22f8d8f40b8d3
S4ika <s4ikait@gmail.com>
2023-03-19 00:20:52 +0300

    Add verbose_name, verbose_name_plural, rename classes, fix admin.py

--- technika_it/client_app/models.py
+++ technika_it/client_app/models.py
@@ -9,45 +9,45 @@ class Image(models.Model):
     class Meta:
         ordering = ["-image_id"]
         verbose_name = "Картинка"
+        verbose_name_plural = "Картинки"
 
     def __str__(self):
         return str(self.image_id)
 
-#Да, его не было на том скрине, который лежит у меня в Pyrus, я его нашел в документации
-class Manufacture(models.Model): #Дал себе вольность убрать множественное число из названия класса
+
+class Manufacture(models.Model):
     manufacture_id = models.IntegerField(primary_key=True)
     title = models.CharField(max_length=100)
 
-    #Возможность сортировки по названию производителя звучит хорошо
+    #Возможность сортировки по названию производителя
     class Meta:
-        ordering = ["-manufacturer_id"]
+        ordering = ["-manufacture_id"]
         verbose_name = "Производитель"
+        verbose_name_plural = "Производители"
 
     def __str__(self):
         return str(self.manufacture_id) + str(self.title)
 
 
-class Categories(models.Model):
+class Category(models.Model):
     category_id = models.IntegerField(primary_key=True)
     title = models.CharField(max_length=75)
 
     class Meta:
         ordering = ["-category_id"]
         verbose_name = "Категория"
+        verbose_name_plural = "Категории"
 
     def __str__(self):
         return str(self.category_id) + str(self.title)
 
 
-class Products(models.Model):
+class Product(models.Model):
     product_id = models.IntegerField(primary_key=True)
-    #Поставил SetNULL при удалении, т.к. не придумал, что может быть другое при удалении картинки
     main_image_id = models.ForeignKey(Image, on_delete=models.SET_NULL, verbose_name="Главная картинка",null=True)
-    #Думал Default поставить при удалении, но в интернетах много страшных вещей про него T_T
-    main_category_id = models.ForeignKey(Categories, on_delete=models.SET_NULL, verbose_name="Категория",null=True)
+    main_category_id = models.ForeignKey(Category, on_delete=models.SET_NULL, verbose_name="Категория",null=True)
     manufacturer_id = models.ForeignKey(Manufacture, on_delete=models.SET_NULL, verbose_name="Производитель",null=True)
     title = models.CharField(max_length=75)
-    #В description заказывали большие поля... воть....
     description = models.TextField()
     price  = models.IntegerField()
     number_of_available = models.IntegerField()
@@ -55,38 +55,42 @@ class Products(models.Model):
     class Meta:
         ordering = ["-price","-product_id","-number_of_available"]
         verbose_name = "Товар"
+        verbose_name_plural = "Товары"
+
 
     def __str__(self):
         return str(self.title) + str(self.price)
 
 
-class Images_products(models.Model):
+class Image_product(models.Model):
     image_id = models.ForeignKey(Image, on_delete=models.SET_NULL, verbose_name="Главная картинка",null=True)
-    product_id = models.ForeignKey(Products, on_delete=models.SET_NULL, verbose_name="Товар",null=True)
-    #Как я понял, в этой таблице требовалось создать общийк ключ, он создается с помощью GenericForeignKey
-    #надеюсь, что это то что нужно =) Для него мы import`нули штуковину
-    id_images_products = GenericForeignKey(
-        'product_id',
-        'image_id',
-    )
+    product_id = models.ForeignKey(Product, on_delete=models.SET_NULL, verbose_name="Товар",null=True)
+    # id_images_products = GenericForeignKey(
+    #     'product_id',
+    #     'image_id',
+    # )
 
     class Meta:
         ordering = ["-product_id"]
+        verbose_name = "Фотография товара"
+        verbose_name_plural = "Фотографии товара"
 
     def __str__(self):
         return str(self.product_id)
 
-#Тут все аналогично с классом выше
-class Categories_products(models.Model):
-    category_id = models.ForeignKey(Categories, on_delete=models.SET_NULL, verbose_name="Категория", null=True)
-    product_id = models.ForeignKey(Products, on_delete=models.SET_NULL, verbose_name="Товар", null=True)
-    id_images_products = GenericForeignKey(
-        'product_id',
-        'category_id',
-    )
+
+class Category_product(models.Model):
+    category_id = models.ForeignKey(Category, on_delete=models.SET_NULL, verbose_name="Категория", null=True)
+    product_id = models.ForeignKey(Product, on_delete=models.SET_NULL, verbose_name="Товар", null=True)
+    # id_images_products = GenericForeignKey(
+    #     'product_id',
+    #     'category_id'
+    # )
 
     class Meta:
         ordering = ["-product_id","-category_id"]
+        verbose_name = "Категория товара"
+        verbose_name_plural = "Категории товара"
 
     def __str__(self):
         return str(self.category_id) + str(self.product_id)
