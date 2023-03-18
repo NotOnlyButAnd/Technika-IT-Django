a1d5343c1c58db93fbe308c3922fff1fd6a693f7
petrichor27 <elisabetbabenko@gmail.com>
2023-03-18 10:35:50 +0300

    Update models.py

--- technika_it/client_app/models.py
+++ technika_it/client_app/models.py
@@ -13,12 +13,12 @@ class Image(models.Model):
     def __str__(self):
         return str(self.image_id)
 
-#Да, его не было на том скрине, который лежит у меня в Pyrus, я его нашел в документации
-class Manufacture(models.Model): #Дал себе вольность убрать множественное число из названия класса
+
+class Manufacture(models.Model): 
     manufacture_id = models.IntegerField(primary_key=True)
     title = models.CharField(max_length=100)
 
-    #Возможность сортировки по названию производителя звучит хорошо
+    #Возможность сортировки по названию производителя 
     class Meta:
         ordering = ["-manufacturer_id"]
         verbose_name = "Производитель"
@@ -41,9 +41,7 @@ class Categories(models.Model):
 
 class Products(models.Model):
     product_id = models.IntegerField(primary_key=True)
-    #Поставил SetNULL при удалении, т.к. не придумал, что может быть другое при удалении картинки
     main_image_id = models.ForeignKey(Image, on_delete=models.SET_NULL, verbose_name="Главная картинка",null=True)
-    #Думал Default поставить при удалении, но в интернетах много страшных вещей про него T_T
     main_category_id = models.ForeignKey(Categories, on_delete=models.SET_NULL, verbose_name="Категория",null=True)
     manufacturer_id = models.ForeignKey(Manufacture, on_delete=models.SET_NULL, verbose_name="Производитель",null=True)
     title = models.CharField(max_length=75)
@@ -63,8 +61,6 @@ class Products(models.Model):
 class Images_products(models.Model):
     image_id = models.ForeignKey(Image, on_delete=models.SET_NULL, verbose_name="Главная картинка",null=True)
     product_id = models.ForeignKey(Products, on_delete=models.SET_NULL, verbose_name="Товар",null=True)
-    #Как я понял, в этой таблице требовалось создать общийк ключ, он создается с помощью GenericForeignKey
-    #надеюсь, что это то что нужно =) Для него мы import`нули штуковину
     id_images_products = GenericForeignKey(
         'product_id',
         'image_id',
@@ -76,7 +72,7 @@ class Images_products(models.Model):
     def __str__(self):
         return str(self.product_id)
 
-#Тут все аналогично с классом выше
+
 class Categories_products(models.Model):
     category_id = models.ForeignKey(Categories, on_delete=models.SET_NULL, verbose_name="Категория", null=True)
     product_id = models.ForeignKey(Products, on_delete=models.SET_NULL, verbose_name="Товар", null=True)
