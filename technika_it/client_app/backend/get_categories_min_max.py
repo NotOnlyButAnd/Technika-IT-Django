6cad38bf01c0fb0427ddac59625072b2ef1c4308
Vika <povilika310121@gmail.com>
2023-03-21 12:48:36 +0300

    Бэк. Получение максимальной и минимальной цены в заданной категории
    Бэк. Получение максимальной и минимальной цены в заданной категории
    Написать метод: получение максимальной и минимальной цены в заданной категории.

---
+++ technika_it/client_app/backend/get_categories_min_max.py
@@ -0,0 +1,28 @@
+import json
+
+
+# @router.get("/new_products/{limit}")
+def get_categories_min_max(url: str = None, category_id: int = None):
+    """
+    олучение максимальной и минимальной цены в заданной категории
+
+    Возвращает файл json
+
+    Запрос sql:
+        SELECT max(products.price), min(products.price)
+        FROM products
+        INNER JOIN categories_products ON categories_products.product_id = products.product_id
+        Where categories_products.category_id={category_id}
+    """
+    data = []
+    products = Products.objects.select_related('images').order_by('-products.product_id')
+    for row in products:
+        data = [
+            {
+                'max': field.max,
+                'min': field.min
+             }
+            for field in row.products.all()
+        ]
+
+    return json.dumps(data)
